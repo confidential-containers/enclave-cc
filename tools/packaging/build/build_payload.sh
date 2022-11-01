3b268cae175ddd95302e3ab38818453b5c7b2708
Mikko Ylinen <mikko.ylinen@intel.com>
2022-11-01 12:55:26 +0200

    tools: build payload without sudo
    Signed-off-by: Mikko Ylinen <mikko.ylinen@intel.com>

--- tools/packaging/build/build_payload.sh
+++ tools/packaging/build/build_payload.sh
@@ -6,7 +6,7 @@ docker rmi $1 -f
 export SCRIPT_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 export ENCLAVE_CC_ROOT="${SCRIPT_ROOT}/../../../"
 
-sudo rm -rf payload_artifacts && sudo mkdir -p payload_artifacts/scripts
+mkdir -p payload_artifacts/scripts
 export PAYLOAD_ARTIFACTS="${SCRIPT_ROOT}/payload_artifacts"
 
 # build pre-installed OCI bundle for agent enclave container
@@ -22,21 +22,19 @@ popd
 # build shim-rune binary: "containerd-shim-rune-v2"
 pushd ${ENCLAVE_CC_ROOT}/src/shim
 make binaries
-sudo cp ./bin/containerd-shim-rune-v2 ${PAYLOAD_ARTIFACTS}
+cp ./bin/containerd-shim-rune-v2 ${PAYLOAD_ARTIFACTS}
 # prepare shim-rune configuration.
-sudo cp ./config/config.toml ${PAYLOAD_ARTIFACTS}/shim-rune-config.toml
+cp ./config/config.toml ${PAYLOAD_ARTIFACTS}/shim-rune-config.toml
 popd
 
-# rune binary will be installed directly through "apt install" inside the docker build.
-
-sudo cp ${SCRIPT_ROOT}/../deploy/enclave-cc-deploy.sh ${PAYLOAD_ARTIFACTS}/scripts
+cp ${SCRIPT_ROOT}/../deploy/enclave-cc-deploy.sh ${PAYLOAD_ARTIFACTS}/scripts
 
 # prepare payload artifacts static tarball
 pushd $PAYLOAD_ARTIFACTS
-sudo tar cfJ enclave-cc-static.tar.xz *
-sudo cp ${SCRIPT_ROOT}/Dockerfile .
+tar cfJ enclave-cc-static.tar.xz *
+cp ${SCRIPT_ROOT}/Dockerfile .
 docker build . -t $1
 popd
 
 #cleanup
-sudo rm -rf payload_artifacts
+rm -rf payload_artifacts
