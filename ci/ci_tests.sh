ea38d4c70854344764811cc9a0ccdc94794bc85c
Liucheng Hu <liucheng.hu@intel.com>
2023-01-11 08:50:13 +0800

    update some details

--- ci/ci_tests.sh
+++ ci/ci_tests.sh
@@ -1,43 +1,57 @@
 #!/bin/bash
 source  ./ci/utils.sh
 
-if [ ! -n "$1" ]; then
-    echo "error: missing input parameter, such as install_coco_operator."
-    exit 1
-fi
-
-function install_coco_operator(){
-    if ! is_cc_operator_controller_manager_pod_exist; then 
-        exit 1
+install_coco_operator() {
+    if is_cc_operator_controller_manager_pod_exist; then 
+        echo "[Error] Found CcCo operator pod."
+        echo "Please uninstall operator pod to ensure a clean CI environment."
+        return 1
     fi
+
     if [ $CI_DEBUG_MODE = true ]; then
         echo "[Debug] Start to install the operator..."
     fi
 
-    logs=$(timeout $TIMEOUT_SECS kubectl apply -k github.com/confidential-containers/operator/config/release?ref=$ECC_RC_VER 2>&1 )
+    logs=$(timeout $TIMEOUT_SECS kubectl apply -k github.com/confidential-containers/operator/config/release?ref=$ECC_RC_VER 2>&1)
     case $? in 
         124)
             echo "[Error] Timeout when installing operator."
             echo "$logs"
-            exit 1
+            return 1
         ;;
         1)
             echo "[Error] Something is wrong when installing operator."
             echo "$logs"
-            exit 1
+            return 1
         ;;
     esac
     
-    if ! check_cc_operator_controller_manager_pod_ready; then 
-        exit 1
+    wait_cc_operator_controller_manager_pod_ready
+    case $? in 
+        124)
+            echo "[Error] Timeout when installing operator."
+            return 1
+        ;;
+        1)
+            echo "[Error] Something is wrong when installing operator."
+            return 1
+        ;;
+    esac
+
+    if [ $CI_DEBUG_MODE = true  ]; then
+        echo "Successfully install CoCo operator."
     fi
-    exit 0
+
+    return 0
 }
 
-function install_enclave_cc_runtimeclass(){
-    if ! is_enclave_cc_runtimeclass_exist; then 
-        exit 1
+install_enclave_cc_runtimeclass() {
+    if is_enclave_cc_runtimeclass_exist; then
+        echo "[Error] Found enclave-cc runtimeclass."
+        echo "Please uninstall enclave-cc runtime to ensure a clean CI environment."
+        return 1
     fi
+
     if [ $CI_DEBUG_MODE = true ]; then
         echo "[Debug] Start to install enclave-cc runtimeclass."
     fi
@@ -47,77 +61,113 @@ function install_enclave_cc_runtimeclass(){
         124)
             echo "[Error] Timeout when installing enclave-cc runtimeclass."
             echo "$logs"
-            exit  1
+            return 1
         ;;
         1)
-            echo "[Error] Something is wrong when installing  enclave-cc runtimeclass."
+            echo "[Error] Something is wrong when installing enclave-cc runtimeclass."
             echo "$logs"
-            exit  1
+            return 1
         ;;
     esac
 
-    if ! check_enclave_cc_runtimeclass_exist; then 
-        exit 1
+    wait_enclave_cc_runtimeclass_ready
+    case $? in
+        124)
+            echo "[Error] Timeout when installing enclave-cc runtimeclass."
+            return 1
+        ;;
+        1)
+            echo "[Error] Something is wrong when installing enclave-cc runtimeclass."
+            return 1
+        ;;
+    esac
+
+    if [ $CI_DEBUG_MODE = true ]; then
+        echo "[OK] Successfully install enclave-cc runtimeclass."
     fi
-    exit  0
+
+    return 0
 }
 
-function uninstall_coco_operator(){
-    if  is_cc_operator_controller_manager_pod_exist; then 
-        exit 1
+uninstall_coco_operator() {
+    if ! is_cc_operator_controller_manager_pod_exist; then 
+        echo "[Error] Not found CoCo operator pod."
+        return 1
     fi
+
     if [ $CI_DEBUG_MODE = true ]; then 
         echo "[Debug] Start to delete operator..."
     fi
+
     logs=$(timeout $TIMEOUT_SECS kubectl delete -k github.com/confidential-containers/operator/config/release?ref=$ECC_RC_VER 2>&1)
     case $? in 
         124)
             echo "[Error] Timeout when uninstalling operator."
             echo "$logs"
-            exit 1
+            return 1
         ;;
         1)   
             echo "[Error] Something is wrong when uninstalling operator."
             echo "$logs"
-            exit 1
+            return 1
         ;;
     esac
+
     if [ $CI_DEBUG_MODE = true ]; then
         echo "[Debug] Successfully delete operator..."
-    fi 
-    exit 0
+    fi
+
+    return 0
 }
 
 function uninstall_enclave_cc_runtimeclass(){
-    if is_enclave_cc_runtimeclass_exist; then 
-        exit 1
+    if ! is_enclave_cc_runtimeclass_exist; then
+        echo "[Error] Not found enclave-cc runtimeclass."
+        return 1
     fi
+
     if [ $CI_DEBUG_MODE = true ]; then
         echo "[Debug] Start to delete enclave-cc runtimeclass..."
+    fi
+
+    # workaround
+    if [ ! -f /opt/confidential-containers/bin ]; then
+        echo "[Debug] The directory doesn't exist. Start to create /opt/confidential-containers/bin dir."
+        mkdir /opt/confidential-containers/bin
     fi
 
     logs=$(timeout $TIMEOUT_SECS kubectl delete -f https://raw.githubusercontent.com/confidential-containers/operator/main/config/samples/enclave-cc/base/ccruntime-enclave-cc.yaml 2>&1)
     case $? in 
        124)
             echo "[Error] Timeout when uninstalling enclave-cc runtimeclass."
-            if [ $CI_DEBUG_MODE = true ]; then
-                echo "[Debug] Start to delete kubernetes stuck CRD deletion..."
-                kubectl get ccruntimes.confidentialcontainers.org -o yaml 2>&1 | sed '/finalizer/{n;d;}'
-                kubectl apply -f /home/ecc/operator/hlc/my-resource.yaml
-            fi
             echo "$logs"
-            exit  0
+
+            # workaround
+            echo "[Debug] Start to delete kubernetes stuck CRD..."
+            kubectl get ccruntimes.confidentialcontainers.org -o yaml | sed '/finalizers/{s/$/[]/;n;d;}' > /home/ecc/operator/hlc/my-resource.yaml
+            kubectl apply -f /home/ecc/operator/hlc/my-resource.yaml
+
+            # return 1
         ;;
         1)
             echo "[Error] Something is wrong when uninstalling enclave-cc runtimeclass."
             echo "$logs"
-            exit 1
+            return 1
         ;;
     esac
+
+    # todo: wait enclave-cc runtimeclass deleted
+
     if [ $CI_DEBUG_MODE = true ]; then
         echo "[Debug] Successfully delete runtimeclass..."
     fi 
-    exit 0
+
+    return 0
 }
+
+if [ $# != 1 ]; then
+    echo "[CI script error] Please specify a task."
+    exit 1
+fi
 
 $1
--- ci/utils.sh
+++ ci/utils.sh
@@ -1,36 +1,16 @@
 #!/bin/bash
+
 ECC_RC_NAME="enclave-cc"
 ECC_RC_VER="v0.2.0"
 TIMEOUT_SECS=120
 CI_DEBUG_MODE=true
 
-function is_enclave_cc_runtimeclass_exist() {
-    ecc_rc_info=$( kubectl get runtimeclass 2>&1 | grep $ECC_RC_NAME  )
-    if [ $? = 0 ]; then
-        echo "Found k8s runtimeclass: "
-        echo "$ecc_rc_info"
-        echo "Please uninstall enclave-cc runtime to ensure a clean CI environment."
-        return 1
-    else
-        echo "Can not found k8s runtimeclass, please install."
-        return 0
-    fi       
-}
-
-function is_cc_operator_controller_manager_pod_exist() {
+is_cc_operator_controller_manager_pod_exist() {
     operator_pod_info=$(kubectl get pods -n confidential-containers-system 2>&1 | grep cc-operator-controller-manager)
-    if [ $? = 0  ]; then
-        echo "Found operator pod :"
-        echo "$operator_pod_info"
-        echo "Please uninstall operator pod to ensure a clean CI environment."
-        return 1
-    else
-        echo "Can not found operator pod, please install."
-        return 0
-    fi  
+    return $?
 }
 
-function check_cc_operator_controller_manager_pod_ready(){
+wait_cc_operator_controller_manager_pod_ready() {
    timeout $TIMEOUT_SECS bash -c '
         CI_DEBUG_MODE=$0
         while [ true ]
@@ -46,49 +26,25 @@ function check_cc_operator_controller_manager_pod_ready(){
             sleep 1
         done
     ' $CI_DEBUG_MODE
-    case $? in 
-        124)
-            echo "[Error] Timeout when installing operator."
-            return 1
-        ;;
-        1)
-            echo "[Error] Something is wrong when installing operator."
-            return 1
-        ;;
-    esac
-    if [ CI_DEBUG_MODE=true ]; then
-        echo "[Debug] Succefully installed the operator."
-    fi
-    return 0
+    return $?
 }
 
-function check_enclave_cc_runtimeclass_exist() {
+is_enclave_cc_runtimeclass_exist() {
+    ecc_rc_info=$(kubectl get runtimeclass 2>&1 | grep $ECC_RC_NAME)
+    return $?
+}
+
+wait_enclave_cc_runtimeclass_ready() {
     timeout $TIMEOUT_SECS bash -c '
-        TIMEOUT_SECS=$0
-        ECC_RC_NAME=$1
-        counter=0
-        while [ $counter -lt $TIMEOUT_SECS ]
+        ECC_RC_NAME=$0
+        while [ true ]
         do
             kubectl get runtimeclass 2>&1 | grep $ECC_RC_NAME > /dev/null
             if [ $? = 0 ]; then
-                exit 0
+                break
             fi
             sleep 1
-            (( counter++ ))
-        done ' $TIMEOUT_SECS $ECC_RC_NAME
-    rtn_code=$?
-    case $rtn_code in
-        124)
-            echo "[Error] Timeout when installing enclave-cc runtimeclass."
-            return 1
-        ;;
-        1)
-            echo "[Error] Something is wrong when installing enclave-cc runtimeclass."
-            return 1
-        ;;
-        0)
-            echo "[OK] Successfully install enclave-cc runtimeclass."
-        ;;
-    esac
-    return 0
+        done 
+    ' $ECC_RC_NAME
+    return $?
 }
